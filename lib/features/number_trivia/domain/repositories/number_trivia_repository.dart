3f0fac4811dfab58859c76ceae34bf79cb80c3c7
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-05 17:33:08 +0200

    solving null safty issues

--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -3,6 +3,6 @@ import 'package:tdd_arc/core/errors/failures.dart';
 import 'package:tdd_arc/features/number_trivia/domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaRespositoy {
-  Future<Either<Failures, NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failures, NumberTrivia>>? getConcreteNumberTrivia(int number);
   Future<Either<Failures, NumberTrivia>> getRandomNumberTrivia();
 }

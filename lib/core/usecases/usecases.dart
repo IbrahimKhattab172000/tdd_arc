fd831be62f507362db3636d64fe1ba6cfcb139cd
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-06 10:44:42 +0200

    Moving NoParams class to core/usecase file to be global

--- lib/core/usecases/usecases.dart
+++ lib/core/usecases/usecases.dart
@@ -1,6 +1,12 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:tdd_arc/core/errors/failures.dart';
 
 abstract class UseCase<Type, Params> {
   Future<Either<Failures, Type>?> call(Params? params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => throw UnimplementedError();
 }

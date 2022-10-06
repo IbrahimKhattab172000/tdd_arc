f3bdbbaad8ab85ddf4ef755f17f7a063b7627cf1
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-06 09:55:42 +0200

    adding some functionalities | Adding Params class

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,14 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:tdd_arc/core/errors/failures.dart';
+import 'package:tdd_arc/core/usecases/usecases.dart';
 import 'package:tdd_arc/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tdd_arc/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRespositoy respositoy;
 
   GetConcreteNumberTrivia(this.respositoy);
 
-  Future<Either<Failures, NumberTrivia>?> execute({required int number}) async {
-    return await respositoy.getConcreteNumberTrivia(number);
+  @override
+  Future<Either<Failures, NumberTrivia>?> call(Params? params) async {
+    return await respositoy.getConcreteNumberTrivia(params!.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  const Params(this.number);
+
+  @override
+  List<Object?> get props => [number];
 }
--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -25,7 +25,7 @@ void main() {
       when(mockNumberTriviaRepository!.getConcreteNumberTrivia(1))
           .thenAnswer((_) async => Right(tNumberTrivia));
       //act
-      final result = await usecase!.execute(number: tNumber);
+      final result = await usecase!.call(Params(tNumber));
       //assert
       expect(result, Right(tNumberTrivia));
       verify(mockNumberTriviaRepository!.getConcreteNumberTrivia(tNumber));
